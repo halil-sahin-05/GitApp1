cdbde394afd713023972baae9ff1c26ebe5c50a6
Halil Sahin <halil.sahin.0005@gmail.com>
2024-05-25 19:55:30 +0300

    v1

---
+++ src/main/java/Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+    public static void main(String[] args) {
+
+        // versiyon 1
+
+        System.out.println("hello git");
+        System.out.println( "git add . : staging aeaya alır");
+    }
+}
